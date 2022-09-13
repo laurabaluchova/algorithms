fa1178ec65548d2586a40cc096bc7cf2fd6fc02c
martinbalucha <martin.balucha@outlook.com>
2022-09-13 21:09:47 +0200

    strangelist first, last and prev defined

--- du01_strangelist_zadani.py
+++ du01_strangelist_zadani.py
@@ -1,3 +1,6 @@
+from cv01_linkedlist_zadani import LinkedList, Node
+
+
 # IB002 Extra domaci ukol 1.
 #
 # Vasi ulohou v tomto priklade je modifikovat jiz existujici strukturu
@@ -35,6 +38,7 @@
 # Definujte datovou strukturu StrangeList.
 # Muzete se inspirovat definici ze zakladniho domaciho ukolu.
 
+
 class StrangeList:
 
     def __init__(self):
@@ -49,8 +53,23 @@ class StrangeList:
 # zakladniho domaciho ukolu.
 
 def list_to_strange_list(linked_list):
-    pass
-    # TODO
+    strange_list = StrangeList()
+
+    linked_list_node = linked_list.first
+    previous_node = None
+    while linked_list_node is not None:
+        strange_list_node = Node()
+        strange_list_node.value = linked_list_node.value
+        strange_list.last = strange_list_node
+
+        if linked_list_node.prev is None:
+            strange_list.first = strange_list_node
+
+        strange_list_node.prev = previous_node
+        previous_node = strange_list_node
+        linked_list_node = linked_list_node.next
+
+    return strange_list
 
 
 # Ukol 3.
@@ -60,3 +79,29 @@ def list_to_strange_list(linked_list):
 def check_strange_list(strange_list):
     pass
     # TODO
+
+
+def test_list_to_strange_list_nonempty():
+    linked_list = LinkedList()
+
+    node1 = Node()
+    node1.value = 1
+
+    node2 = Node()
+    node2.value = 2
+    node1.next = node2
+    node2.prev = node1
+
+    node3 = Node()
+    node3.value = 3
+    node2.next = node3
+    node3.prev = node2
+
+    linked_list.first = node1
+    linked_list.last = node3
+
+    strange_list = list_to_strange_list(linked_list)
+    #TODO: check pointers
+
+
+test_list_to_strange_list_nonempty()
