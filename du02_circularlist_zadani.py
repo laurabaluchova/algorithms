381cf25d2466ad379ca6f680755088772b35a355
martinbalucha <martin.balucha@outlook.com>
2022-09-20 21:08:27 +0200

    finished circular list

--- du02_circularlist_zadani.py
+++ du02_circularlist_zadani.py
@@ -73,9 +73,97 @@ def get_length(node):
 # Pozor na casovou a prostorovou slozitost vaseho algoritmu!
 
 def calculate_opposites(node):
-    # TODO
-    pass
+    length = get_length(node)
 
+    if not is_circular(node) or length % 2 != 0:
+        return node
+
+    nodes_from_current = length // 2
+    actual_node = node
+    opposite_node = get_first_opposite(node, nodes_from_current)
+
+    for i in range(nodes_from_current):
+        actual_node.opposite = opposite_node
+        opposite_node.opposite = actual_node
+        actual_node = actual_node.next
+        opposite_node = opposite_node.next
+    return node
+
+
+def get_first_opposite(node, nodes_from_current):
+    opposite_node = node
+    for i in range(nodes_from_current):
+        opposite_node = opposite_node.next
+    return opposite_node
+
+
+def test_calculate_opposites_circular_single_node():
+    print("Test calculate_opposites with single Node - circular:", end=" ")
+
+    node = Node()
+    node.key = 1
+    node.next = node
+
+    result = calculate_opposites(node)
+
+    if result == node:
+        print("OK")
+    else:
+        print("FALSE")
+
+
+def test_calculate_opposites_linear_multiple_nodes_odd():
+    print("Test calculate_opposites with multiple Nodes - linear, odd:", end=" ")
+
+    node = Node()
+    node.key = 1
+
+    node2 = Node()
+    node2.key = 2
+    node.next = node2
+
+    node3 = Node()
+    node3.key = 3
+    node2.next = node3
+
+    result = calculate_opposites(node)
+
+    if result == node:
+        print("OK")
+    else:
+        print("FALSE")
+
+
+def test_calculate_opposites_circular_multiple_nodes_even():
+    print("Test calculate opposites with multiple Nodes - circular, even:", end=" ")
+
+    node = Node()
+    node.key = 1
+
+    node2 = Node()
+    node2.key = 2
+    node.next = node2
+
+    node3 = Node()
+    node3.key = 3
+    node2.next = node3
+
+    node4 = Node()
+    node4.key = 4
+    node3.next = node4
+    node4.next = node
+
+    result = calculate_opposites(node)
+
+    if node.opposite == node3 and node3.opposite == node and node2.opposite == node4 and node4.opposite == node2:
+        print("OK")
+    else:
+        print("FALSE")
+
+
+test_calculate_opposites_circular_single_node()
+test_calculate_opposites_linear_multiple_nodes_odd()
+test_calculate_opposites_circular_multiple_nodes_even()
 
 """def test_is_circular_none():
     print("Test is_circular with None:", end=" ")
@@ -248,8 +336,8 @@ def test_get_length_linear_multiple_nodes():
         print("FALSE, got: %i, expected: 3" % length)
 
 
-test_get_length_none()
+"""test_get_length_none()
 test_get_length_linear_single_node()
 test_get_length_circular_single_node()
 test_get_length_circular_multiple_nodes()
-test_get_length_linear_multiple_nodes()
+test_get_length_linear_multiple_nodes()"""
